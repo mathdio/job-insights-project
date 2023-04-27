8378ec34f9b6d648c94dc3a857c1c78a22fe7a86
Matheus Diógenes <mathdiogenes@gmail.com>
2023-04-27 19:06:56 -0300

    feat: R11. refactor: test_counter.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,4 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("data/jobs.csv", "Finance") == 512
+    assert count_ocurrences("tests/mocks/jobs.csv", "Time") == 2
